9cf605a923af87a9e803bfd65dde3ac3c1fb6e5c
Julianna Cardenas <julmcardenas@gmail.com>
2021-01-25 23:06:32 -0800

    Finished required user stories

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Julianna Cardenas on 1/25/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
