a18b775b38c7c71f0333f2f7caebc7d4134705bd
Julianna Cardenas <julmcardenas@gmail.com>
2021-02-03 07:37:41 -0800

    Compose, Favorite, Retweet Tweets

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,12 +18,17 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
+//        loadTweets()
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
+    }
     
     @objc func loadTweets() {
         
@@ -44,6 +49,7 @@ class HomeTableViewController: UITableViewController {
             
         }, failure: { (Error) in
             print("Could not retreive tweets!")
+            print(Error)
         })
     }
     
@@ -97,6 +103,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
